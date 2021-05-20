25d9b9bdd59ca336d31aaa255b3d2367293abdc5
Jaan Lambert <jaan.lambert@gmail.com>
2021-05-20 18:06:35 +0300

    Add eslint

--- snowpack.config.js
+++ snowpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
         ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
       },
     ],
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eslint src --ext .js,.jsx,.ts,.tsx',
+      },
+    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
