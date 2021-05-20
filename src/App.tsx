04e7494c38a05d6c978e9405edda849c9142462e
Jaan Lambert <jaan.lambert@gmail.com>
2021-05-20 18:15:41 +0300

    Add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const theme = {
+  colors: {
+    background: '#E5E5E5',
+  },
+}
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0)
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000)
-    return () => clearTimeout(timer)
-  }, [count, setCount])
-  // Return the App component.
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Root></Root>
+    </ThemeProvider>
   )
 }
+
+const Root = styled.main`
+  background-color: ${({ theme }) => theme.colors.background};
+`
 
 export default App
