13205c430b25457b61226806664d7a7a70aeeeca
Jaan Lambert <jaan.lambert@gmail.com>
2021-05-22 11:36:06 +0300

    Add GraphQL

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,1 +1,35 @@
-console.log('hello server')
+import { ApolloServer, gql } from 'apollo-server'
+import userData from './data/users.json'
+
+const typeDefs = gql`
+  enum Role {
+    ACCOUNT_MANAGER
+    ADMIN
+    AGENT
+    EXTERNAL_REVIEWER
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String!
+    role: Role!
+  }
+
+  type Query {
+    getUsers: [User]!
+  }
+`
+
+const resolvers = {
+  Query: {
+    getUsers: () => userData.users
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
